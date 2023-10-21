812411ef170642f27fea3de4388de8eff6a61416
Master <mhmdeveloperofficial@gmail.com>
2023-10-21 04:59:33 +0000

    Revert "Allow setting number of threads during bootstrap via sysprop"
    This reverts commit 056a3a7817fa77aa7d5fca66735a67cd1a661ebc.

--- apexd/apexd.cpp
+++ apexd/apexd.cpp
@@ -1788,14 +1788,8 @@ Result<void> ActivateApexPackages(const std::vector<ApexFileRef>& apexes,
     apex_queue.emplace(&apex);
   }
 
-  size_t worker_num =
-      android::sysprop::ApexProperties::boot_activation_threads().value_or(0);
-
-  // Setting number of workers to the number of packages to load
-  // This seems to provide the best performance
-  if (worker_num == 0) {
-    worker_num = apex_queue.size();
-  }
+  // Creates threads as many as half number of cores for the performance.
+  size_t worker_num = std::max(get_nprocs_conf() >> 1, 1);
   worker_num = std::min(apex_queue.size(), worker_num);
 
   // On -eng builds there might be two different pre-installed art apexes.
